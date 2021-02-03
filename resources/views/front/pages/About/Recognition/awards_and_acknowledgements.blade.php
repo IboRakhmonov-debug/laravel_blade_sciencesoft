31d63707597620da480571c22302f6f8bf8be656
IboRakhmonov-debug <ibrohim.rakhmonov@bk.ru>
2021-02-03 15:31:38 +0500

    ready

--- resources/views/front/pages/About/Recognition/awards_and_acknowledgements.blade.php
+++ resources/views/front/pages/About/Recognition/awards_and_acknowledgements.blade.php
@@ -7,6 +7,85 @@
 @section('main')
 
 
+<div class="w-11/12 mx-auto my-10">
+    <div class="w-full">
+        <div class="text-3xl md:text-4xl  font-bold">
+            ScienceSoft’s Awards and Recognitions
+            <hr class="border-t-2 w-12 border-yellow-600 mt-4 mb-4">
+        </div>
+        <img src="https://www.scnsoft.com/about/awards-recognitions/awards-and-recognitions-sciencesoft-01.svg" class="w-full py-5 " alt="">
+        <div class="w-full p-4">
+            <p class="text-xl leading-10 text-gray-500 md:text-left text-center">For over 31 years, ScienceSoft has supported businesses with high-quality IT services. We help enterprises leverage technologies to keep business processes optimized and bring on digital transformation. We assist product companies with delivering selling software products to the market.</p> <br>
+            <p class="text-xl leading-10 text-gray-500">We take consistently recognized by independent organizations as a trusted and reliable IT services provider, employer and software development company.</p>
+        </div>
+    </div>
+    <div class="w-full flex md:justify-around items-center md:items-start justify-center md:flex-row flex-col">
+        <div class="w-full md:w-1/6 flex justify-center">
+            <div class="w-25 md:w-4/5 h-12 flex justify-center items-center border-gray-400 border-solid border my-3">
+                <p class="px-4 py-2 text-2xl">2020</p>
+            </div>
+        </div>
+        <div class="w-full md:w-4/6 ">
+            <div class="w-full flex">
+                <div class="w-10  flex justify-center">
+                    <div class="w-3 h-3 my-4 bg-blue-700"></div>
+                </div>
+                <div class="w-11/12 ">
+                    <p class="text-lg text-black leading-8 ">
+                        <b>Clutch</b> featured ScienceSoft in the list of <b>Top IT Services Firms</b> and <b>Top 1000 B2B Providers Globally</b> , as well as named us <b>No.1</b> in the lists <b>Top AWS Partners, Top Azure Consultants, Top Salesforce Consultants.</b>
+                    </p>
+                </div>
+            </div>
+            <div class="w-full flex pt-4">
+                <div class="w-10  flex justify-center">
+                    <div class="w-3 h-3 my-2 bg-blue-700"></div>
+                </div>
+                <div class="w-11/12 ">
+                    <p class="text-base text-black  ">
+                        Clutch is known as the premier ratings and reviews platform for buyers of B2B services. Its unbiased ranking is based on a unique methodology that incorporates multiple data points to compare industry leaders.
+                    </p>
+                </div>
+            </div>
+            <div class="w-full flex pt-4">
+                <div class="w-10  flex justify-center">
+                    <div class="w-3 h-3 my-2 bg-blue-700"></div>
+                </div>
+                <div class="w-11/12 ">
+                <p class="text-lg text-black leading-8 ">
+                        <b>TechReviewer</b>  put ScienceSoft <b>first in the list of Leading QA and Software Testing Companies.</b> 
+                    </p>
+                    <p class="text-base text-black  ">
+                        Clutch is known as the premier ratings and reviews platform for buyers of B2B services. Its unbiased ranking is based on a unique methodology that incorporates multiple data points to compare industry leaders.
+                    </p>
+                </div>
+            </div>
+            <div class="w-full flex pt-4">
+                <div class="w-10  flex justify-center">
+                    <div class="w-3 h-3 my-2 bg-blue-700"></div>
+                </div>
+                <div class="w-11/12 ">
+                    <p class="text-lg text-black leading-8 ">
+                        <b>UpFirms.com</b>  included ScienceSoft is the list of <b>Top Web Development Companies.</b> 
+                    </p>
+                    <p class="text-base text-black  ">
+                        Clutch is known as the premier ratings and reviews platform for buyers of B2B services. Its unbiased ranking is based on a unique methodology that incorporates multiple data points to compare industry leaders.
+                    </p>
+                </div>
+            </div>
+        </div>
+        <div class="w-full md:w-1/6 flex justify-center flex-col">
+            <div class="w-28 mx-auto text-center ">
+                <img src="https://www.scnsoft.com/partners/clutch-top-1000-b2b-companies-2020.svg" class="py-2" alt="">
+                <img src="https://www.scnsoft.com/partners/sw-development/clutch-top-it-services-firms.svg" class="py-2" alt="">
+            </div>
+            <img src="https://www.scnsoft.com/partners/managed-services/clutch-top-cloud-consultants.svg" class="w-28 mx-auto py-2" alt="">
+            <img src="https://www.scnsoft.com/partners/sw-development/global-outsourcing-100-logo.svg" class="w-28 mx-auto py-2" alt="">
+            <img src="https://www.scnsoft.com/partners/testing/techreviewer-qa-testing.svg"  class="w-28 mx-auto py-2" alt="">
+            <img src="https://www.scnsoft.com/partners/upfirms-top-web-development-companies.png"  class="w-28 mx-auto py-2" alt="">
+        </div>
+    </div>
+    </div>
+</div>
 
 
 @endsection
